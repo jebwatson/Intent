42c67642f3d51d12a06b8377af3f23f2e53295c2
Jeb Watson <jeb.watson@gmail.com>
2021-05-13 08:06:50 -0500

    Add provider for view dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:intent/providers/DummyHabitProvider.dart';
 import 'package:intent/providers/HabitProvider.dart';
 import 'package:intent/views/HabitList.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(IntentApp());
+  runApp(MultiProvider(providers: [
+    Provider<HabitProvider>(create: (_) => DummyHabitProvider()),
+  ], child: IntentApp()));
 }
 
 class IntentApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final HabitProvider _habitProvider = DummyHabitProvider();
-
     return MaterialApp(
       title: 'Intent',
       theme: ThemeData.dark(),
-      home: HabitsList(habitProvider: _habitProvider),
+      home: HabitsList(habitProvider: context.read<HabitProvider>()),
     );
   }
 }
