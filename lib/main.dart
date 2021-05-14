3ce7e69178ba436f11f3d29959ea5c1c5c307e02
Jeb Watson <jeb.watson@gmail.com>
2021-05-14 10:35:39 -0500

    Use dummy data while debugging

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intent/bloc/habits_bloc.dart';
+import 'package:intent/repositories/habits/dummy_habit_repo.dart';
 import 'package:intent/repositories/habits/firestore_habit_repo.dart';
 import 'package:intent/views/widgets/habit_list.dart';
 import 'package:intent/views/widgets/error_message.dart';
 import 'package:intent/views/widgets/loading_spinner.dart';
+import 'package:flutter/foundation.dart';
 
 void main() {
   runApp(MultiBlocProvider(providers: [
     BlocProvider<HabitsBloc>(
       create: (context) {
-        return HabitsBloc(habitRepository: FirebaseHabitRepo())
+        return HabitsBloc(
+            habitRepository:
+                kReleaseMode ? FirebaseHabitRepo() : DummyHabitRepo())
           ..add(HabitsLoadRequested());
       },
     ),
